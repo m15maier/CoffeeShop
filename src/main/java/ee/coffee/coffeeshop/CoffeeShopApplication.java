20217a485000554db3b64fe8f3afb68ad841fcb9
m15maier <its.m.maier@gmail.com>
2024-02-19 13:49:46 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ee/coffee/coffeeshop/controllers/OrderController.java
    #	src/main/java/ee/coffee/coffeeshop/models/enums/OrderStatus.java
    #	src/main/resources/templates/products.ftlh

--- src/main/java/ee/coffee/coffeeshop/CoffeeShopApplication.java
+++ src/main/java/ee/coffee/coffeeshop/CoffeeShopApplication.java
@@ -1,17 +1,20 @@
 package ee.coffee.coffeeshop;
 
+import ee.coffee.coffeeshop.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableScheduling
 
 public class CoffeeShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeeShopApplication.class, args);
-    }
+        }
 
 }
--- src/main/java/ee/coffee/coffeeshop/services/interfaces/UserService.java
+++ src/main/java/ee/coffee/coffeeshop/services/interfaces/UserService.java
@@ -8,14 +8,10 @@ import java.util.List;
 public interface UserService {
     @Transactional
     User saveUser(String name, String email, String password, String address);
-
     @Transactional
     void saveSecurity(Integer userId, String userEmail, String UserPassword);
-
     User getUserById(Integer id);
-
     List<User> getAllUsers();
     boolean createUser(User user);
-
 
 }
